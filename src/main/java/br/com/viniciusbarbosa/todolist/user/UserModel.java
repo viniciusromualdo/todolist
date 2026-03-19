da18a3bfb937f8f2579c0c8e9b7b897d784bff33
Vinícius Barbosa <vinicius.vrsb@gmail.com>
2026-03-19 17:01:40 -0300

    Primeiros passos para API Todolist

---
+++ src/main/java/br/com/viniciusbarbosa/todolist/user/UserModel.java
@@ -0,0 +1,14 @@
+package br.com.viniciusbarbosa.todolist.user;
+
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String name;
+    private String username;
+    private String password;
+
+
+}
